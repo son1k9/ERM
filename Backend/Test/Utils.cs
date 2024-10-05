f4c788702dc36da5ec1651f448e3e5d9bf6cc858
son1k9 <ultrasonica9@gmail.com>
2024-10-05 17:37:52 +0300

    Add event insert and get tests

--- Backend/Test/Utils.cs
+++ Backend/Test/Utils.cs
@@ -10,8 +10,32 @@ public class Utils
     {
         var db = Directory.CreateTempSubdirectory().FullName + "\\events.db";
         File.Copy(DbFileName, db);
-        File.Copy(DbFileName, db + "-shm");
-        File.Copy(DbFileName, db + "-wal");
+        File.Copy(DbFileName + "-shm", db + "-shm");
+        File.Copy(DbFileName + "-wal", db + "-wal");
         return new SqliteConnectionFactory(db);
     }
+
+    public static User CreateUser()
+    {
+        return new User
+        {
+            Email = "test@test.test",
+            Login = "test_login",
+            Phone = "89209004534",
+            Password = "password"
+        };
+    }
+
+    public static Event CreateEvent()
+    {
+        return new Event
+        {
+            Name = "Test",
+            Description = "TestD",
+            Date = new DateTime(1984, 6, 9),
+            Place = "TestP",
+            Canceled = false,
+        };
+    }
+
 }
