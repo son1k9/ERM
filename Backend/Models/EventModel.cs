98ef5eb4d907b9614102e198d8aeb7ab2e435eee
son1k9 <ultrasonica9@gmail.com>
2024-10-05 15:31:52 +0300

    Implement event model

--- Backend/Models/EventModel.cs
+++ Backend/Models/EventModel.cs
@@ -3,4 +3,99 @@
 public class EventModel(ISqliteConnectionFactory factory)
 {
     readonly ISqliteConnectionFactory factory = factory;
+
+    public async Task<int> Insert(Event _event)
+    {
+        using var connection = factory.GetConnection();
+        await connection.OpenAsync();
+
+        var stmt = @"INSERT INTO event (name, description, date, place, canceled, oraganizer_id) 
+                     VALUES ($name, $description, $date, $place, $canceled, $oraganizer_id);
+                     SELECT last_insert_rowid();";
+
+        var command = connection.CreateCommand();
+        command.CommandText = stmt;
+        command.Parameters.AddWithValue("$name", _event.Name);
+        command.Parameters.AddWithValue("$description", _event.Description);
+        command.Parameters.AddWithValue("$date", _event.Date);
+        command.Parameters.AddWithValue("$place", _event.Place);
+        command.Parameters.AddWithValue("$canceled", _event.Canceled);
+        command.Parameters.AddWithValue("$organizer_id", _event.OraganizerId);
+
+        return Convert.ToInt32(await command.ExecuteScalarAsync());
+    }
+
+    public async Task Update(Event _event)
+    {
+        using var connection = factory.GetConnection();
+        await connection.OpenAsync();
+
+        var stmt = @"UPDATE event
+                     WHERE id = $id
+                     SET 
+                        name = $name,
+                        description = $description,
+                        date = $date,
+                        place = $place,
+                        canceled = $canceled,
+                        organizer_id = $organizer_id;";
+
+        var command = connection.CreateCommand();
+        command.CommandText = stmt;
+        command.Parameters.AddWithValue("$name", _event.Name);
+        command.Parameters.AddWithValue("$description", _event.Description);
+        command.Parameters.AddWithValue("$date", _event.Date);
+        command.Parameters.AddWithValue("$place", _event.Place);
+        command.Parameters.AddWithValue("$canceled", _event.Canceled);
+        command.Parameters.AddWithValue("$organizer_id", _event.OraganizerId);
+
+        await command.ExecuteNonQueryAsync();
+    }
+
+    public async Task<Event?> Get(int id)
+    {
+        using var connection = factory.GetConnection();
+        await connection.OpenAsync();
+
+        var stmt = @"SELECT name, description, date, place, canceled, organizer_id 
+                     FROM event 
+                     WHERE id = $id;";
+
+        var command = connection.CreateCommand();
+        command.CommandText = stmt;
+        command.Parameters.AddWithValue("$id", id);
+
+        using var reader = await command.ExecuteReaderAsync();
+        if (reader.HasRows)
+        {
+            var _event = new Event
+            {
+                Id = reader.GetInt32(0),
+                Name = reader.GetString(1),
+                Description = reader.GetString(2),
+                Date = reader.GetFieldValue<DateTime>(3),
+                Place = reader.GetString(4),
+                Canceled = reader.GetBoolean(5),
+                OraganizerId = reader.GetInt32(6)
+            };
+            return _event;
+        }
+
+        return null;
+    }
+
+    public async Task Delete(int id)
+    {
+        using var connection = factory.GetConnection();
+        await connection.OpenAsync();
+
+        var stmt = @"DELETE FROM event
+                    WHERE id = $id;";
+
+        var command = connection.CreateCommand();
+        command.CommandText = stmt;
+        command.Parameters.AddWithValue("$id", id);
+
+        await command.ExecuteNonQueryAsync();
+    }
 }
--- Backend/Models/UserModel.cs
+++ Backend/Models/UserModel.cs
@@ -1,44 +1,79 @@
-﻿namespace Models;
+﻿using Microsoft.Data.Sqlite;
+
+namespace Models;
 
 public class UserModel(ISqliteConnectionFactory factory)
 {
     readonly ISqliteConnectionFactory factory = factory;
 
-    public async Task Insert(User user)
+    private static User ScanUser(SqliteDataReader reader)
+    {
+        return new User
+        {
+            Id = reader.GetInt32(0),
+            Email = reader.GetString(1),
+            Login = reader.GetString(2),
+            Phone = reader.GetString(3),
+        };
+    }
+
+    public async Task<List<User>> GetUsersForEvent(int eventId)
     {
         using var connection = factory.GetConnection();
-        var stmt = @"INSERT INTO user (email, login, phone, hashed_password) 
-                     VALUES ($email, $login, $phone, $hashed_password)";
+        await connection.OpenAsync();
+
+        var stmt = @"SELECT u.id, u.email, u.login, u.phone
+                    FROM user_event ev INNER JOIN user u ON ev.user_id = u.id  
+                    WHERE ev.event_id = $id;";
+
         var command = connection.CreateCommand();
         command.CommandText = stmt;
-        command.Parameters.AddWithValue("$email", user.Id);
+        command.Parameters.AddWithValue("$id", eventId);
+
+        using var reader = await command.ExecuteReaderAsync();
+        var users = new List<User>();
+        while (await reader.ReadAsync())
+        {
+            users.Add(ScanUser(reader));
+        }
+        return users;
+    }
+
+    public async Task<int> Insert(User user)
+    {
+        using var connection = factory.GetConnection();
+        await connection.OpenAsync();
+
+        var stmt = @"INSERT INTO user (email, login, phone, hashed_password) 
+                     VALUES ($email, $login, $phone, $hashed_password);
+                     SELECT last_insert_rowid();";
+
+        var command = connection.CreateCommand();
+        command.CommandText = stmt;
+        command.Parameters.AddWithValue("$email", user.Email);
         command.Parameters.AddWithValue("$login", user.Login);
-        command.Parameters.AddWithValue("$phone", user.Email);
+        command.Parameters.AddWithValue("$phone", user.Phone);
         var passwordHash = BCrypt.Net.BCrypt.HashPassword(user.Password, 12, false);
         command.Parameters.AddWithValue("$hashed_password", passwordHash);
 
-        await command.ExecuteNonQueryAsync();
+        return Convert.ToInt32(await command.ExecuteScalarAsync());
     }
 
     public async Task<User?> Get(int id)
     {
         using var connection = factory.GetConnection();
-        var stmt = @"SELECT id, email, login, phone FROM user WHERE id = $id";
+        await connection.OpenAsync();
+
+        var stmt = @"SELECT id, email, login, phone FROM user WHERE id = $id;";
+
         var command = connection.CreateCommand();
         command.CommandText = stmt;
         command.Parameters.AddWithValue("$id", id);
 
         using var reader = await command.ExecuteReaderAsync();
-        if (reader.HasRows)
+        if (await reader.ReadAsync())
         {
-            var user = new User
-            {
-                Id = reader.GetInt32(0),
-                Email = reader.GetString(1),
-                Login = reader.GetString(2),
-                Phone = reader.GetString(3)
-            };
-            return user;
+            return ScanUser(reader);
         }
 
         return null;
