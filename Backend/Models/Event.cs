b89c5744a8c9124f4689d6ea856a2f43d6753709
son1k9 <ultrasonica9@gmail.com>
2024-10-06 17:44:30 +0300

    Change fields to properties

--- Backend/Models/Event.cs
+++ Backend/Models/Event.cs
@@ -2,10 +2,10 @@ namespace Models;
 
 public class Event {
     public int Id;
-    public string Name = string.Empty;
-    public string Description = string.Empty;
-    public DateTime Date = DateTime.Now;
-    public string Place = string.Empty;
-    public bool Canceled;
-    public int OraganizerId;
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime Date { get; set; } = DateTime.Now;
+    public string Place { get; set; } = string.Empty;
+    public bool Canceled { get; set; }
+    public int OraganizerId { get; set; }
 }
