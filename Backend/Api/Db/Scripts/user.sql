0e079297a31e5ee1284d1d2dde82705b9880888a
son1k9 <ultrasonica9@gmail.com>
2024-10-11 20:56:24 +0300

    Add tokens model. Change user handlers to require login instead of id. Add authentication handler

--- Backend/Api/Db/Scripts/user.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE user(
-    id INTEGER PRIMARY KEY,
-    email TEXT UNIQUE NOT NULL,
-    login TEXT UNIQUE NOT NULL,
-    hashed_password TEXT NOT NULL
-)
--- Backend/Api/Db/Scripts/user_event.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE user_event(
-    user_id INTEGER NOT NULL,
-    event_id INTEGER NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES user(id),
-    FOREIGN KEY(event_id) REFERENCES event(id),
-    PRIMARY KEY(user_id, event_id)
-)
